125be8611a2e7e2ad2caaeafab3cff8e3b886f27
León ramiro Vallejos <leonrvallejos@gmail.com>
2024-02-27 10:07:11 -0300

    Fix
    Repara problemas del cronometro y unifica funciones de inicio y pausa.

--- appOmodoro.js
+++ appOmodoro.js
@@ -1,46 +1,33 @@
 // CRONOMETRO
 var cronometroInterval;
-var tiempoInicial;
 var segundos = 0;
 var minutos = 0;
 var horas = 0;
-var pausado = false;
-var enEjecución = false;
+var enEjecucion = false;
+//eliminé la var tiempoInicial 
+//eliminé la var pausado
 
-// function iniciarCronometro() {
-//   if (!pausado) {
-//     cronometroInterval = setInterval(actualizarCronometro, 1000);
-//     pausado = false;
-//   }
-// }
-
-//Si está andando el cronometro no podes volver a apretar iniciar capo
-function iniciarCronometro (){
-  if (!enEjecución){
-    cronometroInterval = setInterval (actualizarCronometro, 1000);
-      enEjecución = true;
-      pausado = false;
-    }
+//unificamos las funciones "iniciar y pausar" (anteriormente eran dos diferentes)
+function iniciarPausarCronometro() {
+  if (!enEjecucion) {
+    cronometroInterval = setInterval(actualizarCronometro, 1000); //se inicia el intervalo cronometroInterval que llama a la función actualizarCronometro
+    enEjecucion = true;
+    document.getElementById('start-btn').textContent = 'Pausar'; //el start-btn por defecto es de "iniciar", das click y ahora el text content es "pausar".
+  } else {
+    clearInterval(cronometroInterval);
+    enEjecucion = false;
+    document.getElementById('start-btn').textContent = 'Iniciar';
   }
-
-function pausarCronometro() {
-  clearInterval(cronometroInterval);
-  pausado = true;
 }
-
-// function pausarCronometro(){
-//   clearInterval (cronometroInterval);
-//   enEjecución = false;
-//   pausado = true;
-// }
 
 function reiniciarCronometro() {
   clearInterval(cronometroInterval);
   segundos = 0;
   minutos = 0;
   horas = 0;
+  enEjecucion = false;
+  document.getElementById('start-btn').textContent = 'Iniciar';
   actualizarDisplay();
-  pausado = false;
 }
 
 function actualizarCronometro() {
@@ -64,8 +51,3 @@ function actualizarDisplay() {
 function formatTime(time) {
   return time < 10 ? '0' + time : time;
 }
-
-
-document.getElementById('start-btn').addEventListener('click', iniciarCronometro);
-document.getElementById('pause-btn').addEventListener('click', pausarCronometro);
-document.getElementById('reset-btn').addEventListener('click', reiniciarCronometro);
